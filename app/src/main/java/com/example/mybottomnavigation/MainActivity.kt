d6f507bb262d01cb16e3fccf35dec5d8418682d3
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-05 19:31:18 +0330

    implemented main activity file and logic

--- app/src/main/java/com/example/mybottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/mybottomnavigation/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.mybottomnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mybottomnavigation.ui.theme.MyBottomNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,17 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyBottomNavigation(navController: NavController) {
+    val destinationList = listOf(Login, Settings)
+    val selectedIndex = rememberSaveable { mutableStateOf(0) }
+    NavigationBar {
+        destinationList.forEachIndexed { index, destination ->
+            NavigationBarItem(
+                label = { Text(text = destination.title) },
+                icon = { Icon(imageVector = destination.icon, contentDescription = destination.title) },
+                selected = index == selectedIndex.value,
+                onClick = {
+                    selectedIndex.value = index
+                    navController.navigate(destination.route){
+                        popUpTo(Login.route)
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    MyBottomNavigationTheme {
-        Greeting("Android")
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { MyBottomNavigation(navController = navController)}) {
+        Box(modifier = Modifier.padding(it)){
+            NavHost(navController = navController, startDestination = Login.route ){
+                composable(Login.route){
+                    Login(navController)
+                }
+                composable(Second.route){
+                    Second(navController)
+                }
+                composable(Settings.route){
+                    Settings()
+                }
+            }
+        }
     }
 }
